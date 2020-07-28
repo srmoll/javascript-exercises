ecce8c397e5244449b2d4deccbf8a6f40b59f5b7
Sarah Moll <srmoll@umich.edu>
2020-07-28 11:20:42 -0700

    Used array methods and simple comparison to find the oldest person object from an array of people

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(arr) {
+    
+    if (!('yearOfDeath' in arr[0])) {
+        arr[0]['yearOfDeath'] = new Date().getFullYear();
+    }
+    let first = (arr[0].yearOfDeath - arr[0].yearOfBirth);
+    let eldestIndex = 0;
+    for (let i = 0; i < arr.length; ++i) {
+        if (!('yearOfDeath' in arr[i])) {
+            arr[i]['yearOfDeath'] = new Date().getFullYear();
+        }
+        let next = (arr[i].yearOfDeath - arr[i].yearOfBirth);
+        if (next > first) {
+            first = next;
+            eldestIndex = i;
+        }
+    }
+    return arr[eldestIndex];
 }
 
 module.exports = findTheOldest
