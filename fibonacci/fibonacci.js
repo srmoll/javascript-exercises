7447217632ce07c7e5f9cce9290ec3659126c6ef
Sarah Moll <srmoll@umich.edu>
2020-07-27 22:36:27 -0700

    using non-recursive solution to display nth member of fibonacci series

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = +n;
+    let prev = 0;
+    let next = 1;
+    let nth = 0;
+    if (n < 0) return "OOPS";
+    for (let i = 0; i < n; ++i) {
+        nth = prev + next;
+        prev = next;
+        next = nth;
+    }
+    return prev;
 }
 
 module.exports = fibonacci
