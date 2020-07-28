b311ca472a37e17d1f51374a96e79f66f133703b
Sarah Moll <srmoll@umich.edu>
2020-07-28 11:29:19 -0700

    used map to extract an array of titles from an array of book objects

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (bookArray) {
+	const titles = bookArray.map(book => book.title);
+	return titles;
 }
 
 module.exports = getTheTitles;
